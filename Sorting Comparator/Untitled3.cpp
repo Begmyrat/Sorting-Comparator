6473a53912594dbeb60a3f4b67a49213b81ca282
Begmyrat <39202480+Begmyrat@users.noreply.github.com>
2019-03-06 02:20:23 +0300

    Sorting Comparator

---
+++ Sorting Comparator/Untitled3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int score[1000000];
+string name[1000000];
+
+int main(){
+	
+	cin>>n;
+	
+	for(int i=0;i<n;i++)
+		cin>>name[i]>>score[i];
+	
+	
+	
+	return 0;
+}
